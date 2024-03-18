576e545fe4e3c6bcb12da3e26625c655b165dcf4
gayatrisathawane <gayatripsathawane@gmail.com>
2024-03-18 23:35:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import ButtonList from './Components/ButtonList/ButtonList';
+import Head from './Components/Head/Head';
+
+function App() {
+  return (
+   <div> <Head/>
+   
+   </div>
+  )
+}
+
+
+// Head 
+// Body 
+// sidebar
+// menubar,
+// maincontainer
+// buttonlist
+// videocontainer
+
+export default  App;
---
+++ src/Components/Head/Head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+    head
+    </>
+  )
+}
+
+export default Head
